7495e4a57c881166da381a44198bfa93db35ae72
perkee <hello@perk.ee>
2019-04-27 17:22:13 -0400

    lint but not rewiriting history for it

--- src/Main.elm
+++ src/Main.elm
@@ -1,80 +1,95 @@
+module Main exposing (FloatField, MassUnit(..), Model, Msg(..), Sex(..), abcdef, ffParse, ffValue, init, main, mult, opt, sexDecoder, sexOption, sexToLabel, sexToValue, stringToSexDecoder, stringToUnitDecoder, unitDecoder, unitOption, unitSelect, unitToLabel, unitToValue, update, view, viewInput, viewInputFloat, wilks)
+
+-- (Html, button, div, text, input, option, select)
+
 import Browser
 import Html exposing (..)
--- (Html, button, div, text, input, option, select)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onClick, onInput, on, targetValue)
+import Html.Events exposing (on, onClick, onInput, targetValue)
 import Json.Decode as Json
 
 
 main =
-  Browser.sandbox { init = init, update = update, view = view }
+    Browser.sandbox { init = init, update = update, view = view }
+
 
 abcdef : Sex -> List Float
 abcdef sex =
-  case sex of
-    Male ->
-      [ -216.0475144
-        , 16.2606339
-        , -0.002388645
-        , -0.00113732
-        , 7.01863E-06
-        , -1.291E-08
-      ]
+    case sex of
+        Male ->
+            [ -216.0475144
+            , 16.2606339
+            , -0.002388645
+            , -0.00113732
+            , 7.01863e-6
+            , -1.291e-8
+            ]
 
-    Female ->
-      [
-        594.31747775582
-        , -27.23842536447
-        , 0.82112226871
-        , -0.00930733913
-        , 4.731582E-05
-        , -9.054E-08
-      ]
+        Female ->
+            [ 594.31747775582
+            , -27.23842536447
+            , 0.82112226871
+            , -0.00930733913
+            , 4.731582e-5
+            , -9.054e-8
+            ]
+
 
 mult : Float -> Int -> Float -> Float
 mult bw i c =
-  c * bw ^ (toFloat i)
+    c * bw ^ toFloat i
+
 
 wilks : Sex -> Maybe Float -> Maybe Float -> String
 wilks sex mbw mw =
-  sex
-  |> abcdef
-  |> List.indexedMap (mult (Maybe.withDefault 0 mbw))
-  |> List.foldl (+) 0
-  |> \denom -> (Maybe.withDefault 0 mw) * 500 / denom
-  |> String.fromFloat
-      
+    sex
+        |> abcdef
+        |> List.indexedMap (mult (Maybe.withDefault 0 mbw))
+        |> List.foldl (+) 0
+        |> (\denom ->
+                Maybe.withDefault 0 mw
+                    * 500
+                    / denom
+                    |> String.fromFloat
+           )
 
 
-type MassUnit = KG | LBM
-type Sex = Male | Female
+type MassUnit
+    = KG
+    | LBM
+
+
+type Sex
+    = Male
+    | Female
+
 
 type alias FloatField =
-  { value : Maybe Float
-  , input : String
-  }
+    { value : Maybe Float
+    , input : String
+    }
 
 
 type alias Model =
-  { mass : FloatField
-  , massUnit : MassUnit
-  , bodyMass : FloatField
-  , bodyMassUnit : MassUnit
-  , sex : Sex
-  }
+    { mass : FloatField
+    , massUnit : MassUnit
+    , bodyMass : FloatField
+    , bodyMassUnit : MassUnit
+    , sex : Sex
+    }
 
 
 init : Model
 init =
-  Model (FloatField (Just 0.0) "0") LBM (FloatField (Just 0.0) "0") LBM Male
+    Model (FloatField (Just 0.0) "0") LBM (FloatField (Just 0.0) "0") LBM Male
 
 
 type Msg
-  = SetMass String
-  | SetBodyMass String
-  | SetMassUnit MassUnit
-  | SetBodyMassUnit MassUnit
-  | SetSex Sex
+    = SetMass String
+    | SetBodyMass String
+    | SetMassUnit MassUnit
+    | SetBodyMassUnit MassUnit
+    | SetSex Sex
 
 
 ffValue : FloatField -> Float
@@ -92,24 +107,23 @@ ffParse ff str =
             FloatField (Just new) str
 
 
-
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    SetMass s ->
-      { model | mass = ffParse model.mass s }
+    case msg of
+        SetMass s ->
+            { model | mass = ffParse model.mass s }
 
-    SetBodyMass s ->
-      { model | bodyMass = ffParse model.bodyMass s }
+        SetBodyMass s ->
+            { model | bodyMass = ffParse model.bodyMass s }
 
-    SetMassUnit u ->
-      { model | massUnit = u }
+        SetMassUnit u ->
+            { model | massUnit = u }
 
-    SetBodyMassUnit u ->
-      { model | bodyMassUnit = u }
+        SetBodyMassUnit u ->
+            { model | bodyMassUnit = u }
 
-    SetSex s ->
-      { model | sex = s }
+        SetSex s ->
+            { model | sex = s }
 
 
 unitDecoder : Json.Decoder MassUnit
@@ -121,33 +135,35 @@ unitDecoder =
 
 stringToUnitDecoder : String -> Json.Decoder MassUnit
 stringToUnitDecoder s =
-  case s of
-    "KG" ->
-      Json.succeed KG
+    case s of
+        "KG" ->
+            Json.succeed KG
 
-    "LBM" ->
-      Json.succeed LBM
+        "LBM" ->
+            Json.succeed LBM
 
-    x ->
-      Json.fail ("unknown unit" ++ x)
+        x ->
+            Json.fail ("unknown unit" ++ x)
 
 
 unitToValue : MassUnit -> String
 unitToValue mu =
-  case mu of
-    KG ->
-      "KG"
-    LBM ->
-      "LBM"
+    case mu of
+        KG ->
+            "KG"
+
+        LBM ->
+            "LBM"
 
 
 unitToLabel : MassUnit -> String
 unitToLabel mu =
-  case mu of
-    KG ->
-      "Kilos"
-    LBM ->
-      "Pounds"
+    case mu of
+        KG ->
+            "Kilos"
+
+        LBM ->
+            "Pounds"
 
 
 sexDecoder : Json.Decoder Sex
@@ -156,83 +172,86 @@ sexDecoder =
     targetValue
         |> Json.andThen stringToSexDecoder
 
+
 stringToSexDecoder : String -> Json.Decoder Sex
 stringToSexDecoder s =
-  case s of
-    "M" ->
-      Json.succeed Male
+    case s of
+        "M" ->
+            Json.succeed Male
 
-    "F" ->
-      Json.succeed Female
+        "F" ->
+            Json.succeed Female
 
-    x ->
-      Json.fail ("unknown sex" ++ x)
+        x ->
+            Json.fail ("unknown sex" ++ x)
 
 
 sexToValue : Sex -> String
 sexToValue s =
-  case s of
-    Male ->
-      "M"
-    Female ->
-      "F"
+    case s of
+        Male ->
+            "M"
+
+        Female ->
+            "F"
 
 
 sexToLabel : Sex -> String
 sexToLabel s =
-  case s of
-    Male ->
-      "Man"
-    Female ->
-      "Woman"
+    case s of
+        Male ->
+            "Man"
+
+        Female ->
+            "Woman"
 
 
 opt : (a -> String) -> (a -> String) -> a -> Html msg
 opt valToValue valToLabel val =
-  option [ val |> valToValue |> value ] [ val |> valToLabel |> text ]
+    option [ val |> valToValue |> value ] [ val |> valToLabel |> text ]
 
 
 unitOption : MassUnit -> Html msg
 unitOption =
-  opt unitToValue unitToLabel
+    opt unitToValue unitToLabel
 
 
 sexOption : Sex -> Html msg
 sexOption =
-  opt sexToValue sexToLabel
+    opt sexToValue sexToLabel
 
 
 unitSelect : (MassUnit -> Msg) -> Html Msg
 unitSelect m =
-  select
-    [ on "change" (Json.map m unitDecoder) ]
-    [
-      unitOption KG,
-      unitOption LBM
-    ]
+    select
+        [ on "change" (Json.map m unitDecoder) ]
+        [ unitOption KG
+        , unitOption LBM
+        ]
 
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ select
-      [ on "change" (Json.map SetSex sexDecoder)
-      ]
-      [ sexOption Male
-      , sexOption Female
-      ]
-    , viewInput "number" "0" model.mass.input SetMass
-    , unitSelect SetMassUnit
-    , viewInput "number" "0" model.bodyMass.input SetBodyMass
-    , unitSelect SetBodyMassUnit
-    , span [] [ wilks model.sex model.mass.value model.bodyMass.value |> text ]
-    ]
+    div []
+        [ select
+            [ on "change" (Json.map SetSex sexDecoder)
+            ]
+            [ sexOption Male
+            , sexOption Female
+            ]
+        , viewInput "number" "0" model.mass.input SetMass
+        , unitSelect SetMassUnit
+        , viewInput "number" "0" model.bodyMass.input SetBodyMass
+        , unitSelect SetBodyMassUnit
+        , span [] [ wilks model.sex model.mass.value model.bodyMass.value |> text ]
+        ]
 
 
 viewInput : String -> String -> String -> (String -> msg) -> Html msg
 viewInput t p v toMsg =
-  input [ type_ t, placeholder p, value v, onInput toMsg ] []
+    input [ type_ t, placeholder p, value v, onInput toMsg ] []
+
 
 viewInputFloat : String -> Float -> (Float -> msg) -> Html msg
 viewInputFloat p v toMsg =
-  viewInput "number" p (String.fromFloat v) (String.toFloat >> Maybe.withDefault 0.0 >> toMsg)
+    viewInput "number" p (String.fromFloat v) (String.toFloat >> Maybe.withDefault 0.0 >> toMsg)
